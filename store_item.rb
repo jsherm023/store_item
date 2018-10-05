52e2b731ecb1bcafa1d8ac931148078eefa161db
“Joe <joseph.sherman@Loop.colum.edu>
2018-10-04 19:45:56 -0500

    added attr_reader and attr_writer.

--- store_item.rb
+++ store_item.rb
@@ -9,6 +9,9 @@
 # p "#{guitar1[:brand_name]} #{guitar1[:model]} from #{guitar1[:year]}"
 
 class Guitar
+  attr_reader :brand_name, :model_name, :item_year
+  attr_writer :item_year
+
   def initialize (input_brand_name, input_model_name, input_item_year)
     @brand_name = input_brand_name
     @model_name = input_model_name
@@ -16,21 +19,9 @@ class Guitar
 
   end
 
-  def brand_name
-    @brand_name
-  end
-
-  def model_name
-    @model_name
-  end
-
-    def item_year
-    @item_year
-  end
-
-  def item_year=(input_year)
-    @item_year = input_year
-  end
+  # def item_year=(input_year)
+  #   @item_year = input_year
+  # end
 
   def print_info
     p "#{brand_name} #{model_name} #{item_year}"
@@ -46,4 +37,5 @@ end
     guitar1.print_info
     guitar2.print_info
     guitar3.print_info
+
   
